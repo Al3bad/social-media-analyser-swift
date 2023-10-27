33b92568d6f8f9024a880e062dc5ccce2f3192a2
Abdullah Alabbad <abdullah@alabbad.dev>
2023-10-27 19:16:27 +1100

    update .gitignore

---
+++ SocialMediaAnalyserGUI/SocialMediaAnalyserGUI/SocialMediaAnalyserGUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaAnalyserGUIApp.swift
+//  SocialMediaAnalyserGUI
+//
+//  Created by Abdullah Alabbad on 27/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaAnalyserGUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
