c6ece0bc8b6789e038cbd0845402718d8d1f659f
Abdullah Alabbad <abdullah@alabbad.dev>
2023-11-03 17:44:20 +1100

    delete user by admin and prevent deleting admin users

--- SocialMediaAnalyserGUI/SocialMediaAnalyserGUI/View/UsersView.swift
+++ SocialMediaAnalyserGUI/SocialMediaAnalyserGUI/View/UsersView.swift
@@ -9,12 +9,29 @@ import SwiftUI
 import SwiftData
 
 struct UsersView: View {
+    @Environment(\.modelContext) var ctx
+    
+    @State private var showingAlert = false
+    
     @Query var users: [User]
     
     var body: some View {
         NavigationStack {
             List(users) { user in
                 Text("\(user.firstName) \(user.lastName) (\(user.username))")
+                    .foregroundStyle(.red)
+                    .swipeActions {
+                        Button("Delete", systemImage: "trash", role: .destructive) {
+                            if user.isAdmin {
+                                showingAlert = true
+                            } else {
+                                ctx.delete(user)
+                            }
+                        }
+                    }
+                    .alert(isPresented: $showingAlert) {
+                        Alert(title: Text("Action Not Allowed"), message: Text("Admin user cannot be deleted!"), dismissButton: .default(Text("Ok")))
+                    }
             }
         }
     }
